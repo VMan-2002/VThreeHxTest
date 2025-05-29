afc33418699e028913d5aecc06d806acc5278a9c
VMan-2002 <vman.2002.au@gmail.com>
2025-05-29 11:30:34 +1000

    Initial commit

---
+++ ThreeHx2Test/source/PlayState.hx
@@ -0,0 +1,34 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	/*var cube;
+	
+	override public function create()
+	{
+		super.create();
+
+		var scene = new Scene();
+		var camera = new PerspectiveCamera(75, FlxG.width / FlxG.height, 0.1, 1000);
+
+		var renderer = new WebGLRenderer();
+		renderer.setSize(FlxG.width, FlxG.height);
+		add(renderer.flixelSprite());
+
+		var geometry = new BoxGeometry(1, 1, 1);
+		var material = new MeshBasicMaterial({color: 0x00ff00});
+		cube = new Mesh(geometry, material);
+		scene.add(cube);
+		camera.position.z = 5;
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+	}*/
+}
