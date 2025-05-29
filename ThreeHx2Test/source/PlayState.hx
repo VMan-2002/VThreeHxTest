c030606bfcf180c25b3504971c6d5c5bd47ca9c2
VMan-2002 <vman.2002.au@gmail.com>
2025-05-29 15:33:40 +1000

    blah

--- ThreeHx2Test/source/PlayState.hx
+++ ThreeHx2Test/source/PlayState.hx
@@ -2,16 +2,15 @@ package;
 
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
-	/*var cube;
+class PlayState extends FlxState {
+	var cube;
+	var text:FlxText;
 	
-	override public function create()
-	{
+	override public function create() {
 		super.create();
 
 		var scene = new Scene();
-		var camera = new PerspectiveCamera(75, FlxG.width / FlxG.height, 0.1, 1000);
+		/*var camera = new PerspectiveCamera(75, FlxG.width / FlxG.height, 0.1, 1000);
 
 		var renderer = new WebGLRenderer();
 		renderer.setSize(FlxG.width, FlxG.height);
@@ -21,14 +20,19 @@ class PlayState extends FlxState
 		var material = new MeshBasicMaterial({color: 0x00ff00});
 		cube = new Mesh(geometry, material);
 		scene.add(cube);
-		camera.position.z = 5;
+		camera.position.z = 5;*/
+
+		add(text = new FlxText(0, 0, 0, "It works"));
 	}
 
-	override public function update(elapsed:Float)
-	{
+	override public function update(elapsed:Float) {
 		super.update(elapsed);
 
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
-	}*/
+		text.x += 1;
+		if (text.x >= 200)
+			text.x = 0;
+		
+		/*cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;*/
+	}
 }
